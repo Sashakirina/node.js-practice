3a7e7096e3cf9bc5b0a8e72aaeb6bcc28d25ba51
OksanaGukova <kseniagukova@ukr.net>
2024-09-08 16:05:17 +0300

    task4

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getAllProductController } from '../controllers/products.js';
+import { getAllProductController, getProductByIdController } from '../controllers/products.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 router.get('/products', ctrlWrapper(getAllProductController));
+
+router.get('/products/:productId', ctrlWrapper(getProductByIdController));
 
 export default router;
--- src/services/products.js
+++ src/services/products.js
@@ -2,3 +2,9 @@ import { ProductModel } from '../db/Product.js';
 export const getAllProducts = async () => {
   return await ProductModel.find();
 };
+
+
+export const getProductById = async (productId) => {
+  const product = await ProductModel.findById(productId);
+  return product;
+}
