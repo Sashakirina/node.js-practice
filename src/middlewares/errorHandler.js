e24ce50dc1dc62f894c7414af43ba16704630884
Oleksandra <sashakirina@gmail.com>
2024-10-06 14:04:42 +0200

    get add

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,16 +1,14 @@
-import {HttpError} from 'http-errors'
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (error, req, res, next) => {
   console.log('my errors', error);
 
   if (error instanceof HttpError) {
-    res.status(error.status).json(
-      {
-        status: error.status,
-        message: error.message,
-        data: error
-  }
-)
+    res.status(error.status).json({
+      status: error.status,
+      message: error.message,
+      data: error,
+    });
   }
   res.status(500).json({
     status: 500,
