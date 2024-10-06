b29c9b6338d51b98e501b995f3166002aec0ead8
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-10-06 16:34:16 +0300

    completed task

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ import cors from 'cors';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import productsRouter from './routers/products.js';
-import userRouter from './routers/auth.js'
+import userRouter from './routers/auth.js';
 
 import { env } from './utils/env.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -14,8 +15,10 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
-app.use(userRouter);
+  app.use(userRouter);
   app.use(productsRouter);
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
